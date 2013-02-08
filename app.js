5c9f38d9e3747b00c3ef7858311782f00b27040a
Brendan Molloy <brendan@bbqsrc.net>
2013-02-09 01:23:34 +1100

    kgo

---
+++ app.js
@@ -0,0 +1,18 @@
+angular.module("yvc", []);
+
+function YourVoteCountsCtrl($scope) {
+    $scope.candidates = ['A', 'B', 'C', 'D'];
+    $scope.preference = 1;
+    $scope.ballots = [,
+        [[1,1,1,1], [2,2,2], [3,3], [0,0,0,0,0,0]],
+        [[1,1,1,1], [3,3,2,2,2], [], [0,0,0,0,0,0]],
+        [[], [1,1,3,3,2,2,2], [], [1,1,0,0,0,0,0,0]],
+        [[], [], [], [1,1,3,3,2,2,2,1,1,0,0,0,0,0,0]]
+    ];
+    $scope.getColor = function(cand) {
+        return "cand-" + cand;
+    }
+    $scope.incrementPref = function() {
+        $scope.preference = Math.min($scope.preference+1, $scope.ballots.length-1);
+    }
+}
