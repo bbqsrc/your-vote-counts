add5207819d7dd71648baa56743ef7fe24cbf3d5
Brendan Molloy <brendan@bbqsrc.net>
2013-02-09 01:27:11 +1100

    fuu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ angular.module("yvc", []);
 function YourVoteCountsCtrl($scope) {
     $scope.candidates = ['A', 'B', 'C', 'D'];
     $scope.preference = 1;
-    $scope.ballots = [
+    $scope.ballots = [[],
         [[1,1,1,1], [2,2,2], [3,3], [0,0,0,0,0,0]],
         [[1,1,1,1], [3,3,2,2,2], [], [0,0,0,0,0,0]],
         [[], [1,1,1,3,3,2,2,2], [], [1,0,0,0,0,0,0]],
